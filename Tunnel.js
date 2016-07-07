ed195fdb72cd812676af839d83aa18d75adba900
nickidebruyn <nickidebruyn@gmail.com>
2016-07-07 20:29:54 +0200

    color

--- Tunnel.js
+++ Tunnel.js
@@ -3,7 +3,7 @@ function Tunnel(x, y, s, strSize) {
 	var yPos = y;
 	var scaleVal = 1;
 	var size = s;
-	var velocity = 0.02;
+	var velocity = 0.03;
 	var strokeSize = strSize;
 	
 	this.render = function() {
@@ -11,7 +11,7 @@ function Tunnel(x, y, s, strSize) {
 		ellipseMode(CENTER);
 		noFill();
 		strokeWeight(strokeSize*scaleVal);
-		stroke(255*(1-scaleVal));
+		stroke(255*(1-scaleVal), 0, 255*scaleVal);
 		ellipse(xPos, yPos, size*scaleVal, size*scaleVal);
 		// rect(xPos, yPos, size*scaleVal, size*scaleVal, 30);
 		pop();
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var tunnels = [];
-var maxTunnels = 60;
+var maxTunnels = 30;
 var sizeSlider;
 var strokeSlider;
 
@@ -7,23 +7,23 @@ var sizeLabel;
 var strokeLabel;
 
 function setup() {
-  createCanvas(1280, 720);
+  createCanvas(windowWidth, windowHeight);
   
   sizeLabel = createSpan('Size:');
   sizeLabel.position(10, 10);
-  sizeSlider = createSlider(50, 300, 200);
+  sizeSlider = createSlider(50, 300, 120);
   sizeSlider.position(100, 10);
   
   strokeLabel = createSpan('Stroke:');
   strokeLabel.position(10, 50);
-  strokeSlider = createSlider(1, 30, 2);
+  strokeSlider = createSlider(1, 10, 4);
   strokeSlider.position(100, 50);
   
 }
 
 function draw() {
 	
-	background(255);
+	background(0);
 	
 	tunnels.push(new Tunnel(mouseX, mouseY, sizeSlider.value(), strokeSlider.value()));
 	
@@ -38,4 +38,8 @@ function draw() {
 		
 	}
   
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
