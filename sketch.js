ee709b781185cc5af19b02a499d0cbf8f38059bd
nickidebruyn <nickidebruyn@gmail.com>
2016-07-08 11:44:12 +0200

    Bubbles

--- sketch.js
+++ sketch.js
@@ -1,43 +1,35 @@
-var tunnels = [];
-var maxTunnels = 30;
-var sizeSlider;
-var strokeSlider;
-
-var sizeLabel;
-var strokeLabel;
+var bubbles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  sizeLabel = createSpan('Size:');
-  sizeLabel.position(10, 10);
-  sizeSlider = createSlider(50, 300, 120);
-  sizeSlider.position(100, 10);
-  
-  strokeLabel = createSpan('Stroke:');
-  strokeLabel.position(10, 50);
-  strokeSlider = createSlider(1, 10, 4);
-  strokeSlider.position(100, 50);
-  
 }
 
 function draw() {
+	background(18, 52, 86);
 	
-	background(0);
+	//Random placement of bubbles
+	if (random(1) < 0.05) {
+		bubbles.push(new Bubble(random(0, width), height, random(50, 100)));
+	}
 	
-	tunnels.push(new Tunnel(mouseX, mouseY, sizeSlider.value(), strokeSlider.value()));
-	
-	if (tunnels.length >= maxTunnels) {
-		tunnels.splice(0, 1);
+	for (var i=0; i<bubbles.length; i++) {
+		bubbles[i].update();
+		bubbles[i].render();
 		
 	}
-
-	//Draw all tunnels
-	for (i = 0; i < tunnels.length; i++) {
-		tunnels[i].render();
+	
+	//Check if bubbles can be remove
+	for (var i=bubbles.length-1; i>=0; i--) {
+		
+		if (bubbles[i].isDead()) {
+			bubbles.splice(i, 1);
+			
+		}
 		
 	}
-  
+	
+	console.log("bubble count = " + bubbles.length);
 }
 
 function windowResized() {
