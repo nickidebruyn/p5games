322569bf1c2b0241eec9a0e38fe0deabd0d1d4f0
nickidebruyn <nickidebruyn@gmail.com>
2016-08-05 22:32:04 +0200

    ui

--- sketch.js
+++ sketch.js
@@ -1,19 +1,36 @@
 var bubbles = [];
 
-var button1;
+var addTileButton;
+var tileTextField;
+var tileWidthField;
+var tileHeightField;
+var tileXPosField;
+var tileYPosField;
+
+var selectedTile;
+var tiles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  //button1 = createDiv();
-  
+	tileTextField = new InputField(20, 20, "Tile Text:", "Tile");
+	tileWidthField = new InputField(20, 40, "Tile Width", "200");
+	tileHeightField = new InputField(20, 60, "Tile Height", "200");
+	
+	tileXPosField = new InputField(20, 80, "Tile X Pos", "0");
+	tileYPosField = new InputField(20, 100, "Tile Y Pos", "0");
+
+	addTileButton = createButton("Add");
+	addTileButton.position(20, 120);
+	addTileButton.mousePressed(addNewTile);
+
 }
 
 function draw() {
 	background(18, 52, 86);
 	
 	//Random placement of bubbles
-	if (random(1) < 0.05) {
+	if (random(1) < 0.01) {
 		bubbles.push(new Bubble(random(0, width), height, random(50, 100)));
 	}
 	
@@ -33,7 +50,32 @@ function draw() {
 		
 	}
 	
+	if (selectedTile != null) {
+		// console.log("width: " + );
+		selectedTile.position(mouseX - selectedTile.width / 2, mouseY - selectedTile.height / 2);
+
+	}
+	
 	// console.log("bubble count = " + bubbles.length);
+}
+
+function addNewTile() {
+	var tile = new TileButton(tileWidthField.getValue(), 
+	tileHeightField.getValue(), tileTextField.getValue());
+	tile.centerAt(0, 0);
+	tile.addTouchDownListener(selectTile);
+	tile.addTouchUpListener(unselectTile);
+	tiles.push(tile);
+	
+}
+
+function selectTile() {
+	selectedTile = this;
+}
+
+function unselectTile() {
+	selectedTile = null;
+
 }
 
 function windowResized() {
